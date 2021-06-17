66205fe68a9e1d234e2b1f108fafd5a4fec09278
BorisGautier <tchoukouahaboris@outlook.fr>
2021-06-17 06:21:43 +0100

    Update Architecture

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      $('.loading-apps').hide();
+    }, 3000);
   }
-
 }
