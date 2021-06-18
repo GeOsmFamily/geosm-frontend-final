d5cfb31da948eb1d6ca2378f3d8d345b56f032c8
BorisGautier <tchoukouahaboris@outlook.fr>
2021-06-18 17:11:11 +0100

    Logo & Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'geosm-frontend';
+  environment;
+
+  public constructor(private titleService: Title) {
+    this.environment = environment;
+  }
+  ngOnInit(): void {
+    this.setTitle();
+  }
+
+  public setTitle() {
+    this.titleService.setTitle('GeOsm ' + environment.nom.toLowerCase());
+  }
 }
