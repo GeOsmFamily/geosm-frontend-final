ddfb43ff76ead23cfd5ca2587a32876a22bdfd12
BorisGautier <tchoukouahaboris@outlook.fr>
2021-06-22 15:35:57 +0100

    Share Functionnality Fixes #31

---
+++ src/app/components/modal/descriptive-sheet-modal/descriptive-sheet-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descriptive-sheet-modal',
+  templateUrl: './descriptive-sheet-modal.component.html',
+  styleUrls: ['./descriptive-sheet-modal.component.scss']
+})
+export class DescriptiveSheetModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
