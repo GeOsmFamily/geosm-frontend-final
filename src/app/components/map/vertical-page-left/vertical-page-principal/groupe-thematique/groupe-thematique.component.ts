58107ed90efed7b78d6dcc92014e075476f4b7e8
BorisGautier <tchoukouahaboris@outlook.fr>
2021-06-22 07:50:24 +0100

    Add Badge thematique

--- src/app/components/map/vertical-page-left/vertical-page-principal/groupe-thematique/groupe-thematique.component.ts
+++ src/app/components/map/vertical-page-left/vertical-page-principal/groupe-thematique/groupe-thematique.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GroupThematiqueInterface } from 'src/app/interfaces/groupeInterface';
 import { environment } from 'src/environments/environment';
 
@@ -7,10 +7,17 @@ import { environment } from 'src/environments/environment';
   templateUrl: './groupe-thematique.component.html',
   styleUrls: ['./groupe-thematique.component.scss'],
 })
-export class GroupeThematiqueComponent {
+export class GroupeThematiqueComponent implements OnInit {
   @Input() groupThematique: GroupThematiqueInterface | undefined;
 
   url_prefix: string = environment.url_prefix;
+
+  length;
+
+  ngOnInit(): void {
+    //@ts-ignore
+    this.length = this.groupThematique?.sous_thematiques.length;
+  }
 
   constructor() {}
 }
