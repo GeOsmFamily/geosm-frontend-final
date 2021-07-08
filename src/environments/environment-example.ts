06d3e71bc23faf9818c49fb9dd6a6decb6ebcbd8
BorisGautier <tchoukouahaboris@outlook.fr>
2021-07-08 01:09:46 +0100

    update project

---
+++ src/environments/environment-example.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  global_logo: undefined,
+  primaryColor: '#023f5f',
+  url_prefix: 'url_backend/',
+  url_frontend: 'urlFrontend/',
+  url_service: 'https://service.geo.sm/',
+  path_qgis: '/var/www/geosm/',
+  pojet_nodejs: 'pojet_nodejs_value',
+  nom: 'nomInstance',
+  countrycode: 'code_country',
+  avaible_language: ['fr', 'en'],
+  default_language: 'langue',
+};
